d6bf2ec74c2aa7419d6c18a6949540f582e0c4c8
Breno Almeida <kbreno08@gmail.com>
2021-02-25 21:11:28 -0300

    Refatoração da UserController. Criação de testes automatizados.

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 const usercontroller = new UserController();
+const surveyscontroller = new SurveysController();
 
 router.post("/users", usercontroller.create);
+router.post("/surveys", surveyscontroller.create);
+router.get("/surveys", surveyscontroller.show);
 
 export { router };
