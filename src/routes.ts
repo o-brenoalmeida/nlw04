7c7e24656f5b7815b5f60831a223c406e76806d8
Breno Almeida <kbreno08@gmail.com>
2021-02-27 10:40:53 -0300

    Aula 4

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
@@ -6,9 +7,12 @@ const router = Router();
 
 const usercontroller = new UserController();
 const surveyscontroller = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post("/users", usercontroller.create);
 router.post("/surveys", surveyscontroller.create);
 router.get("/surveys", surveyscontroller.show);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
