2a51aa9c63454779ff951cdfadccf6bf548ca09d
Breno Almeida <kbreno08@gmail.com>
2021-02-28 14:36:01 -0300

    Aula 5

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,26 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from 'yup';
+import { AppError } from "../errors/AppError";
 
 class UserController {
 
-    async create(request: Request, response: Response){
-        const {name, email} = request.body;
+    async create(request: Request, response: Response) {
+        const { name, email } = request.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required("Nome é obrigatório"),
+            email: yup.string().email().required("E-mail incorreto")
+        })
+
+        //validation
+        try {
+            await schema.validate(request.body, { abortEarly: false });
+        } catch (err) {
+            throw new AppError(err)
+
+        }
 
         const usersRepository = getCustomRepository(UsersRepository);
 
@@ -13,17 +28,15 @@ class UserController {
             email,
         });
 
-        if(userAlreadyExists){
-            return response.status(400).json({
-                error: "User already exists!",
-            });
+        if (userAlreadyExists) {
+            throw new AppError("User already exists!")
         }
 
-       const user =  usersRepository.create({
-           name, email
-       })
+        const user = usersRepository.create({
+            name, email
+        })
 
-       await usersRepository.save(user);
+        await usersRepository.save(user);
 
 
         return response.status(201).json(user);
--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,24 @@
 import { Router } from 'express';
+import { AnswerController } from './controllers/AnswerController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
+import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
 const usercontroller = new UserController();
 const surveyscontroller = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.post("/users", usercontroller.create);
 router.post("/surveys", surveyscontroller.create);
 router.get("/surveys", surveyscontroller.show);
 
 router.post("/sendMail", sendMailController.execute);
+router.get("/answers/:value", answerController.execute);
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
